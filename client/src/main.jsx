9406402701cfb9be14362adab67e8b4f1d289db6
Maria :) <88095943+maliraa07@users.noreply.github.com>
2026-06-02 20:56:56 -0300

    Wrap App in StrictMode and LanguageProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { LanguageProvider } from "./i18n/LanguageContext.jsx";
 import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
-<BrowserRouter basename="/guiropa-systems">
-  <App />
-</BrowserRouter>
+  <StrictMode>
+    <BrowserRouter basename="/guiropa-systems">
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
