0a9fe8cfe08025b65080c185afe4fced2765d145
HafenConsol <info.hafen@proton.me>
2026-08-26 10:39:53 -0300

    SATURATE GUIROPA WITH GLOBAL COMMERCIAL INVENTORY

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -3,6 +3,7 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import ScrollToTop from "./ScrollToTop.jsx";
 import GuiropaCommercialLayer from "./GuiropaCommercialLayer.jsx";
+import GuiropaCommercialFooterDeck from "./GuiropaCommercialFooterDeck.jsx";
 
 export default function Layout() {
   return (
@@ -15,6 +16,7 @@ export default function Layout() {
         <Outlet />
       </div>
 
+      <GuiropaCommercialFooterDeck />
       <Footer />
     </>
   );
